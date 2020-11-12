5d7110a0848f391d8731ab3fe8d4714032509e8f
zhanguohuang <zhanguohuang@bytedance.com>
2020-11-12 15:30:05 +0800

    finish 1-18 and 1-19

--- practice/chapter1/1-18delSpaceAndTab.c
+++ practice/chapter1/1-18delSpaceAndTab.c
@@ -2,17 +2,51 @@
 // Created by bytedance on 2020/11/11.
 //
 
+// 编写一个程序，删除每个输入行末尾的空格及制表符，并删除完全是空格的行。
 #include <stdio.h>
 
 #define MAXLINE 1000
 
 int getLine(char line[], int lim);
+// 删除末尾是空格的行
+int removeSpace(char line[]);
 
 int main() {
-    int n;
     char line[MAXLINE];
-    // 怎么区别是空行还是结束了呢
-    while((n = getLine(line, MAXLINE)) > 0) {
-
+    while(getLine(line, MAXLINE) > 0) {
+        if (removeSpace(line) > 0) {
+            printf("%s", line);
+        }
     }
+}
+
+int getLine(char s[], int lim) {
+    int c, i;
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        s[i] = c;
+    }
+
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+int removeSpace(char line[]) {
+    int i;
+    i = 0;
+    while (line[i] != '\n')
+        ++i;
+    while (i > 0 && (line[i] == '\t' || line[i] == ' ')) {
+        --i;
+    }
+    if (i > 0) {
+        ++i;
+        line[i] = '\n';
+        ++i;
+        line[i] = '\0';
+    }
+    return i;
 }
--- practice/chapter1/1-19reverse(s).c
+++ practice/chapter1/1-19reverse(s).c
@@ -4,3 +4,61 @@
 
 //编写函数reverse(s)，将字符串s中的字符顺序颠倒过来。
 //使用该函数编写一个程序，每次颠倒一个输入行中的字符顺序。
+
+#include <stdio.h>
+#define MAXLINE 1000
+
+int getLine(char s[], int lim);
+void revertLine(char s[]);
+void revertLinePlus(char s[]);
+
+int main() {
+    char line[MAXLINE];
+    while (getLine(line, MAXLINE) > 0) {
+        revertLine(line);
+        printf("%s", line);
+    }
+}
+
+int getLine(char s[], int lim) {
+    int c, i;
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        s[i] = c;
+    }
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void revertLine(char s[]) {
+    int i;
+    i = 0;
+    while (s[i] != '\n') {
+        i++;
+    }
+    for (int j = 0; j < (i + 1) / 2; j++) {
+        char temp;
+        temp = s[j];
+        s[j] = s[i-j];
+        s[i-j] = temp;
+    }
+}
+
+void revertLinePlus(char s[]) {
+    int i, j;
+    char temp;
+    i = j = 0;
+    while (s[i] != '\n') {
+        i++;
+    }
+    while (j < i) {
+        temp = s[j];
+        s[j] = s[i];
+        s[i] = s[j];
+        j++;
+        i--;
+    }
+}
