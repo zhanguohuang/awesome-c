11141a9b58aa42602af6ebc9e90d0c01043b138b
zhanguohuang <zhanguohuang@bytedance.com>
2020-11-16 09:53:22 +0800

    2-1 signed_unsigned_range

--- practice/chapter2/2-1signed_unsigned_range.c
+++ practice/chapter2/2-1signed_unsigned_range.c
@@ -8,4 +8,11 @@
 #include <limits.h>
 #include <float.h>
 
+#include <stdio.h>
 
+int main() {
+    printf("signed: char_min:%d, char_max:%d", CHAR_MIN, CHAR_MAX);
+    printf("unsigned: unsigned_char_min:%d, unsigned_char_max:%d", 0, UCHAR_MAX);
+    printf("signed: int_min:%d, int_max:%d", INT_MIN, INT_MAX);
+    printf("signed: char_min:%d, char_max:%d", CHAR_MIN, CHAR_MAX);
+}
